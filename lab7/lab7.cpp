71641b118030b4cbad9475f2e5b83d8e648a977b
ISMAEL ISAAC VASQUEZ PEREZ <ivasquezp5@miumg.edu.gt>
2025-02-21 22:53:25 -0600

    codigo ismael

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -1,203 +1,113 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-
-// Clase base
-class CuentaBancaria {
-protected:
-    string titular;
-    double saldo;
-public:
-    CuentaBancaria(string t, double s) : titular(t), saldo(s) {}
-
-    virtual void depositar(double cantidad) {
-        saldo += cantidad;
-        cout << "Depositados Q" << cantidad << ". Nuevo saldo: Q" << saldo << endl;
-    }
-
-    virtual void retirar(double cantidad) {
-        if (cantidad <= saldo) {
-            saldo -= cantidad;
-            cout << "Retirados Q" << cantidad << ". Nuevo saldo: Q" << saldo << endl;
-        }
-        else {
-            cout << "Fondos insuficientes.\n";
-        }
-    }
-
-    virtual void mostrarInformacion() const {
-        cout << "Titular: " << titular << "\nSaldo: Q" << saldo << endl;
-    }
-
-    virtual ~CuentaBancaria() {}
-};
-
-// Subclase Cuenta de Ahorro
-class CuentaAhorro : public CuentaBancaria {
+class Profesor
+{
 private:
-    double tasaInteres;
+	string cursos;
+	string titulo;
 public:
-    CuentaAhorro(string t, double s, double tasa) : CuentaBancaria(t, s), tasaInteres(tasa) {}
+	Profesor(string cursos, string titulo) {
+		this->cursos = cursos;
+		this->titulo = titulo;
 
-    void aplicarInteres() {
-        double interes = saldo * tasaInteres / 100;
-        saldo += interes;
-        cout << "Interes aplicado: Q" << interes << ". Nuevo saldo: Q" << saldo << endl;
-    }
-
-    void mostrarInformacion() const override {
-        CuentaBancaria::mostrarInformacion();
-        cout << "Tasa de interes: " << tasaInteres << "%\n";
-    }
+	}
+	void mostrardatos() {
+		cout << "Cursos: " << cursos << endl;
+		cout << "Titulo: " << titulo << endl;
+	};
+	void getcurso(string cursos) {
+		this->cursos = cursos;
+	};
+	void gettitulo(string titulo) {
+		this->titulo = titulo;
+	};
 };
-
-// Subclase Cuenta Corriente
-class CuentaCorriente : public CuentaBancaria {
+class catedratico : private Profesor
+{
 private:
-    double sobregiro;
+	string nombre;
+	string apellido;
+
 public:
-    CuentaCorriente(string t, double s, double sg) : CuentaBancaria(t, s), sobregiro(sg) {}
+	catedratico(string nombre, string apellido, string cursos, string titulo) : Profesor(cursos, titulo) {
+		this->nombre = nombre;
+		this->apellido = apellido;
+	}
+	void mostrardatos1() {
+		cout << "Nombre: " << nombre << endl;
+		cout << "Apellido: " << apellido << endl;
+		Profesor::mostrardatos();
+	};
 
-    void retirar(double cantidad) override {
-        if (cantidad <= saldo + sobregiro) {
-            saldo -= cantidad;
-            cout << "Retirados Q" << cantidad << ". Nuevo saldo: Q" << saldo << endl;
-        }
-        else {
-            cout << "Limite de sobregiro excedido.\n";
-        }
-    }
 
-    void mostrarInformacion() const override {
-        CuentaBancaria::mostrarInformacion();
-        cout << "Limite de sobregiro: Q" << sobregiro << "\n";
-    }
+
 };
-
-// Subclase Cuenta Plazo Fijo
-class CuentaPlazoFijo : public CuentaBancaria {
+class aniosclase : public catedratico
+{
 private:
-    int mesesRestantes;
+	int anios;
+	string clase;
 public:
-    CuentaPlazoFijo(string t, double s, int meses) : CuentaBancaria(t, s), mesesRestantes(meses) {}
+	aniosclase(int anios, string clase, string nombre, string apellido, string cursos, string titulo) : catedratico(nombre, apellido, cursos, titulo) {
+		this->anios = anios;
+		this->clase = clase;
+	}
 
-    void retirar(double cantidad) override {
-        if (mesesRestantes > 0) {
-            cout << "No puede retirar fondos antes de " << mesesRestantes << " meses.\n";
-        }
-        else {
-            CuentaBancaria::retirar(cantidad);
-        }
-    }
+	void mostrardatos2() {
+		system("cls");
+		cout << "Anios dando clase: " << anios << endl;
+		cout << "Clase que daba: " << clase << endl;
+		catedratico::mostrardatos1();
+	};
 
-    void reducirMes() {
-        if (mesesRestantes > 0) mesesRestantes--;
-    }
+};
+class estudiantesacarg :public aniosclase
+{
+private:
+	string estudiantegrado;
+	int cantidad;
+public:
+	estudiantesacarg(string estudiantegrado, int cantidad, int anios, string clase, string nombre, string apellido, string cursos, string titulo) : aniosclase(anios, clase, nombre, apellido, cursos, titulo) {
+		this->estudiantegrado = estudiantegrado;
+		this->cantidad = cantidad;
+	}
+	void mostrardatos3() {
+		system("cls");
+		cout << "Estudiantes a cargo: " << estudiantegrado << endl;
+		cout << "Cantidad de estudiantes: " << cantidad << endl;
+		aniosclase::mostrardatos2();
+	};
 
-    void mostrarInformacion() const override {
-        CuentaBancaria::mostrarInformacion();
-        cout << "Meses restantes para retiro: " << mesesRestantes << "\n";
-    }
+
 };
 
-// Funcion principal
 int main() {
-    int tipoCuenta;
-    string nombre;
-    double saldoInicial;
+	string nombre, apellido, curso, titulo, clase, estudiantesacar;
+	int anios, cantidad;
 
-    cout << "Bienvenido al sistema bancario.\n";
-    cout << "Ingrese su nombre: ";
-    getline(cin, nombre);
+	cout << "Ingrese el nombre del catedratico: ";
+	getline(cin, nombre);
 
-    cout << "Ingrese saldo inicial: Q";
-    cin >> saldoInicial;
+	cout << "Ingrese el apellido del catedratico: ";
+	getline(cin, apellido);
 
-    cout << "Seleccione el tipo de cuenta:\n";
-    cout << "1. Cuenta de Ahorro\n";
-    cout << "2. Cuenta Corriente\n";
-    cout << "3. Cuenta Plazo Fijo\n";
-    cout << "Opcion: ";
-    cin >> tipoCuenta;
+	cout << "Ingrese los cursos que imparte (separados por comas): ";
+	getline(cin, curso);
 
-    CuentaBancaria* cuenta = nullptr;
+	cout << "Ingrese el titulo del profesor: ";
+	getline(cin, titulo);
+	cout << "ingrese anios dando clase: ";
+	cin >> anios;
+	cout << "ingrese la clase que daba: ";
+	cin >> clase;
+	cout << "ingrese estudiantes a cargo: ";
+	getline(cin, estudiantesacar);
+	cout << "ingrese cantidad de estudiantes: ";
+	cin >> cantidad;
+	estudiantesacarg catedratico1(estudiantesacar, cantidad, anios, clase, nombre, apellido, curso, titulo);
+	catedratico1.mostrardatos3();
 
-    if (tipoCuenta == 1) {
-        double tasa;
-        cout << "Ingrese la tasa de interes (%): ";
-        cin >> tasa;
-        cuenta = new CuentaAhorro(nombre, saldoInicial, tasa);
-    }
-    else if (tipoCuenta == 2) {
-        double sobregiro;
-        cout << "Ingrese el limite de sobregiro: Q";
-        cin >> sobregiro;
-        cuenta = new CuentaCorriente(nombre, saldoInicial, sobregiro);
-    }
-    else if (tipoCuenta == 3) {
-        int meses;
-        cout << "Ingrese los meses de plazo: ";
-        cin >> meses;
-        cuenta = new CuentaPlazoFijo(nombre, saldoInicial, meses);
-    }
-    else {
-        cout << "Opcion invalida. Saliendo...\n";
-        return 0;
-    }
-
-    // Menu de operaciones
-    int opcion;
-    do {
-        cout << "\nOperaciones disponibles:\n";
-        cout << "1. Depositar dinero\n";
-        cout << "2. Retirar dinero\n";
-        if (tipoCuenta == 1) cout << "3. Aplicar interes (Solo Cuenta Ahorro)\n";
-        if (tipoCuenta == 3) cout << "4. Reducir meses (Solo Cuenta Plazo Fijo)\n";
-        cout << "5. Mostrar informacion\n";
-        cout << "6. Salir\n";
-        cout << "Seleccione una opcion: ";
-        cin >> opcion;
-
-        double cantidad;
-        switch (opcion) {
-        case 1:
-            cout << "Ingrese cantidad a depositar: Q";
-            cin >> cantidad;
-            cuenta->depositar(cantidad);
-            break;
-        case 2:
-            cout << "Ingrese cantidad a retirar: Q";
-            cin >> cantidad;
-            cuenta->retirar(cantidad);
-            break;
-        case 3:
-            if (tipoCuenta == 1) {
-                dynamic_cast<CuentaAhorro*>(cuenta)->aplicarInteres();
-            }
-            else {
-                cout << "Opcion invalida.\n";
-            }
-            break;
-        case 4:
-            if (tipoCuenta == 3) {
-                dynamic_cast<CuentaPlazoFijo*>(cuenta)->reducirMes();
-                cout << "Mes reducido.\n";
-            }
-            else {
-                cout << "Opcion invalida.\n";
-            }
-            break;
-        case 5:
-            cuenta->mostrarInformacion();
-            break;
-        case 6:
-            cout << "Saliendo...\n";
-            break;
-        default:
-            cout << "Opcion invalida. Intente de nuevo.\n";
-        }
-    } while (opcion != 6);
-
-    delete cuenta; // Liberar memoria
-    return 0;
+	return 0;
 }
